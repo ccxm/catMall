2e290480d5054307623a2582ea9b8cac70f160cd
喵 <wx_8ea8f7e8d0754389a4e69568921ec666@git.code.tencent.com>
2019-09-09 18:36:38 +0800

    v 2019/09/09 18:36

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,47 @@
 <template>
-
+  <div class="login">
+    <el-dialog
+      title="登录"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "login"
+        name: "login",
+        data() {
+            return {
+                dialogVisible: true
+            };
+        },
+        methods: {
+            handleClose(done) {
+                this.$confirm('确认关闭？')
+                    .then(_ => {
+                        done();
+                    })
+                    .catch(_ => {
+                    });
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss">
+  .login {
+
+    .el-dialog__title {
+      font-size: 26px }
+
+  }
+
 
 </style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -2,13 +2,19 @@
   <div>
     <el-header>
       <h1>Welcome to cxm Mall!</h1>
+      <login></login>
     </el-header>
   </div>
 </template>
 
 <script>
+    import login from "../components/login"
+
     export default {
-        name: "index"
+        name: "index",
+        components: {
+            login
+        }
     }
 </script>
 
